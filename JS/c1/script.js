12dce1375bf5a95af7cd2820cb1639c3bef81a80
nag2mani <nag2mani@gmail.com>
2024-07-20 16:58:57 +0530

    buld on off done

--- JS/c1/script.js
+++ JS/c1/script.js
@@ -1,1 +1,7 @@
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
+// document.getElementById('demo').innerHTML = 'Hello JavaScript';
+
+// function onbulb(){
+//     document.getElementById('ddt').src = 'https://www.w3schools.com/js/pic_bulbon.gif';
+// }
+
+
