2c66f62cdbf36f9b2105a0489725a62b3fe3e2fe
nag2mani <nag2mani@gmail.com>
2024-08-27 14:55:11 +0530

    destructure done

---
+++ React/test.js
@@ -0,0 +1,16 @@
+let colors1 = ['red', 'green', 'blue', 'yellow', 'pink'];
+
+// Destructuring with skipping
+let [firstColor, , thirdColor] = colors1;
+
+console.log(firstColor); // red
+console.log(thirdColor); // blue
+
+
+
+let colors = ['red', 'green', 'blue', 'yellow', 'pink']
+
+
+let [f, , , ,s] = colors;
+console.log(f)
+console.log(s)
