f3a374b9104d47a324eb166e4e887d391b753558
nag2mani <nag2mani@gmail.com>
2024-08-31 11:35:51 +0530

    rest is doing

--- JS/test.js
+++ JS/test.js
@@ -1,16 +1,18 @@
-let colors1 = ['red', 'green', 'blue', 'yellow', 'pink'];
-
-// Destructuring with skipping
-let [firstColor, , thirdColor] = colors1;
-
-console.log(firstColor); // red
-console.log(thirdColor); // blue
+// let colors1 = ['red', 'green', 'blue', 'yellow', 'pink'];
+// // Destructuring with skipping
+// let [firstColor, , thirdColor] = colors1;
+// console.log(firstColor); // red
+// console.log(thirdColor); // blue
 
 
+// let colors = ['red', 'green', 'blue', 'yellow', 'pink']
+// let [f, , , ,s] = colors;
+// console.log(f)
+// console.log(s)
 
-let colors = ['red', 'green', 'blue', 'yellow', 'pink']
 
+const { a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4 };
+console.log(a); // 1
+console.log(b); // 2
+console.log(rest); // { c: 3, d: 4 }
 
-let [f, , , ,s] = colors;
-console.log(f)
-console.log(s)
