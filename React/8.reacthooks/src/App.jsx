73c6b39ca55d3a5b72eecdc779d2d28578a932be
Nagmani kumar <nag2mani@gmail.com>
2024-09-23 12:56:08 +0530

    added useeffect

--- React/8.reacthooks/src/App.jsx
+++ React/8.reacthooks/src/App.jsx
@@ -2,12 +2,21 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import useFetch from './useFetch';
-
+import Navbar from './assets/Navbar';
 
 function App() {
-  // const [data, setdata] = useState("jsdhba");
 
-  // useEffect(<function>, <dependency>)
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState(0)
+
+
+  useEffect(() => {
+    alert("Count was changed")
+    setColor(color + 1)
+  }, [count])
+
+
+  // // useEffect(<function>, <dependency>)
   // useEffect(()=>{
   //   fetch('https://jsonplaceholder.typicode.com/todos/180')
   //   .then((res) => res.json())
@@ -17,14 +26,21 @@ function App() {
   //   })
   // }, [setdata])
 
-  const [data] = useFetch('https://jsonplaceholder.typicode.com/todos/180');
+  // // const [data] = useFetch('https://jsonplaceholder.typicode.com/todos/180');
 
   return (
     <>
-    <div className="container">
+      <Navbar color={"navy " + "blue" + color} />
+      <h1>React Useeffect</h1>
+      <div className='container'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    {/* <div className="container">
       <h1>API Data Goes Here</h1>
       <p>{data}</p>
-    </div>
+    </div> */}
     </>
   )
 }
